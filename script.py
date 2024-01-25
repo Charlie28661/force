27f2b779d06816cb7f46f9d423a28c15c2f760a4
Charlie <87935084+Charlie28661@users.noreply.github.com>
2024-01-25 10:57:46 +0800

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,6 @@ import requests
 times = int(input("Enter a number to try guess flag ==>"))
 
 for i in range(1, times):
-    url = 'http://127.0.0.1:8600/{}'.format(i)
+    url = 'http://10.201.202.66:8600/{}'.format(i)
     response = requests.get(url).text
     print(response)
